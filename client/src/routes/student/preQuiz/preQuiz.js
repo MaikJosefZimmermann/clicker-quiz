77a7a39ce367b03892fc879e87fe441e10723ad4
maikzimmermann <mz059@hdm-stuttgart.de>
2016-02-18 16:26:31 +0100

    preQuiz hinzugefügt (Warteraum)

---
+++ client/src/routes/student/preQuiz/preQuiz.js
@@ -0,0 +1,26 @@
+/**
+ * Created by maikzimmermann on 18.02.16.
+ */
+(function () {
+    'use strict';
+
+    angular
+        .module('app.preQuiz', [])                                     // creates new module
+        .config(config)                                             // config function for our module app.list
+        .controller('preQuizCtrl', preQuizCtrl);                          // bind ListCtrl to module
+
+
+    function config($stateProvider) {
+        // inject $stateProvider into config object
+        $stateProvider
+            .state('preQuiz', {                                        // declare list view
+                url: '/preQuiz/',                                       // set url
+                templateUrl: 'routes/student/preQuiz/preQuiz.html',           // defines the HTML template
+                controller: 'preQuizCtrl as vm'                              // this view shall use the ListCtrl previously declared.
+            });
+    }
+
+    function preQuizCtrl($stateParams, $http, $state, $timeout, socket) {// our controller for this view
+
+    }
+})();
