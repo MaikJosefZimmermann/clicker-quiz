5e6e520aedcae4f294228073bcf04db953f1852a
maikzimmermann <mz059@hdm-stuttgart.de>
2016-03-11 15:39:50 +0100

    $scope durch vm ersetzt

--- client/src/routes/lecturer/questionSingle/questionSingle.js
+++ client/src/routes/lecturer/questionSingle/questionSingle.js
@@ -13,12 +13,12 @@
             .state('questionedit', {                        // edit state..
                 url: '/questionedit/:id',                   // url is '/edit/'+id as a url parameter ( check line  32 to see how we use the id with $stateParams
                 templateUrl: 'routes/lecturer/questionSingle/questionSingle.html',       // defines the HTML template
-                controller: 'questionEditCtrl'              // this view shall use the EditCtrl previously declared.
+                controller: 'questionEditCtrl as vm'              // this view shall use the EditCtrl previously declared.
             })
             .state('questionadd', {                         // add view
                 url: '/questionadd',                        // this time without any parameters in the url
                 templateUrl: 'routes/lecturer/questionSingle/questionSingle.html',   // loads the HTML template
-                controller: 'questionAddCtrl'               // this view shall use the AddCtrl previously declared.
+                controller: 'questionAddCtrl as vm'               // this view shall use the AddCtrl previously declared.
             });
 
     }
@@ -26,22 +26,22 @@
     var chips;
 
 
-    function questionEditCtrl($stateParams, $scope, $http, $state, $localStorage) {    // inject stuff into our Ctrl Function so that we can use them.
-
-        $scope.edit = true;                                     // set the scope variable "edit" to true, anything that is within the scope is accessible from within the html template. See single.html line #5, ng if uses this
+    function questionEditCtrl($stateParams, $http, $state, $localStorage) {    // inject stuff into our Ctrl Function so that we can use them.
+        var vm = this;
+        vm.edit = true;                                     // set the scope variable "edit" to true, anything that is within the scope is accessible from within the html template. See single.html line #5, ng if uses this
 
         $http({                                                 // http get requst to our api passing the id. this will load a specific user object
             method: 'GET',
             url: '/api/questions/' + $stateParams.id
         }).then(function successCallback(response) {          // hint: async! when the data is fetched we do ..
-            $scope.question = response.data;                        // load the response data to the scope.user obj
-            setTags($scope.question.tags);
-            $scope.question.creator = $localStorage.user;
+            vm.question = response.data;                        // load the response data to the scope.user obj
+            setTags(vm.question.tags);
+            vm.question.creator = $localStorage.user;
 
         });
 
 
-        $scope.delete = function () {                           // declare a scope function ( which is also accessible from html template)
+        vm.delete = function (id) {                           // declare a scope function ( which is also accessible from html template)
             $http({                                             // if button (single.html line 44) is clicked this function will send a DELETE request to our node server and passes the id
                 method: 'DELETE',
                 url: '/api/questions/' + $stateParams.id
@@ -50,11 +50,23 @@
             });
         };
 
-        $scope.questionSave = function () {
-            saveTags($scope.question);                             // another scope function that will save a user object to our nodejs server
+        /* vm.delete = function (id) {                           // declare a scope function ( which is also accessible from html template)
+
+         $http({                                             // if button (single.html line 44) is clicked this function will send a DELETE request to our node server and passes the id
+         method: 'DELETE',
+         url: '/api/questions/' + id
+         }) .then(function successCallback(response) {
+
+         // when the server responses we rediret to the list
+         });
+         $state.go('questionList');
+         };*/
+
+        vm.questionSave = function () {
+            saveTags(vm.question);                             // another scope function that will save a user object to our nodejs server
             $http({
                 method: 'PUT',                                  // hint: learn http request verbs: get, put (change), delete
-                data: $scope.question,                              // this passes the data from the user object  to the request.
+                data: vm.question,                              // this passes the data from the user object  to the request.
                 url: '/api/questions/' + $stateParams.id
             }).then(function successCallback(response) {
                 $state.go('questionList');
@@ -62,19 +74,19 @@
         };
     }
 
-    function questionAddCtrl($scope, $http, $state, $localStorage) {
+    function questionAddCtrl($http, $state, $localStorage) {
+        var vm = this;
+        vm.new = true;                                       // counterpart to line 28 to set apart whether edit or save operations should be displayed in the view.
 
-        $scope.new = true;                                       // counterpart to line 28 to set apart whether edit or save operations should be displayed in the view.
-
-        $scope.questionSave = function () {                        // for new users we only need the save function
-            saveTags($scope.question);
-            console.log($scope.question);
-            $scope.question.creator = $localStorage.user;
+        vm.questionSave = function () {                        // for new users we only need the save function
+            saveTags(vm.question);
+            console.log(vm.question);
+            vm.question.creator = $localStorage.user;
 
 
             $http({                                              // same as in the EditCtrl
                 method: 'POST',
-                data: $scope.question,
+                data: vm.question,
                 url: '/api/questions'
             }).then(function successCallback(response) {
                 $state.go('questionList');
